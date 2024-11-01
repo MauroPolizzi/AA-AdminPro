9bb9b96da6d4c46ff8d5b4e980d88637119a41c6
MauroPolizzi <mauropolizzi2@gmail.com>
2024-10-31 23:53:24 -0300

    Seccion 18 - mostrar informacion de busqueda global

--- src/app/Services/search/search.service.ts
+++ src/app/Services/search/search.service.ts
@@ -27,6 +27,14 @@ export class SearchService {
       );
   }
 
+  public buscarAll(query: string) {
+    
+    const url = `${base_url}/todo/${query}`;
+
+    return this.http.get<any[]>(url, { headers: this.authService.headers })
+      .pipe();
+  }
+
   // Transformamos la data, pasandola por los operadores pipe y map
   // esto para ir retornando un nuevo obj con su respectivo tipo
   public transformarData(tipo: string, data: any[]): any[]{
--- src/app/pages/busqueda/busqueda.component.ts
+++ src/app/pages/busqueda/busqueda.component.ts
@@ -1,5 +1,9 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../../Services/search/search.service';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { MedicoModel } from 'src/app/models/medico.model';
+import { HospitalModel } from 'src/app/models/hospital.model';
 
 @Component({
   selector: 'app-busqueda',
@@ -7,11 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./busqueda.component.css']
 })
 export class BusquedaComponent implements OnInit {
+  
+  public hospitales: any[] = [];
+  public medicos: any[] = [];
+  public usuarios: any[] = [];
 
-  constructor(private activateRoute: ActivatedRoute) { }
+  constructor(private activateRoute: ActivatedRoute, private searchService: SearchService) { }
 
   ngOnInit(): void {
-    this.activateRoute.params.subscribe( resp => console.log(resp));
+    this.activateRoute.params.subscribe( ({termino}) => this.busquedaGlobal(termino));
+  }
+
+  public busquedaGlobal(termino: string) {
+    
+    this.searchService.buscarAll(termino)
+      .subscribe( (resp: any) => {
+        console.log(resp);
+        this.hospitales = resp.hospitales;
+        this.medicos = resp.medicos;
+        this.usuarios = resp.usuarios;
+      });
   }
 
 }
